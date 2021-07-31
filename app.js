74c2cfc652452eb65bc2b7d6c4e0b4c71a218d3e
skullKim <space1878@naver.com>
2021-07-31 17:59:06 +0900

    추가: 새로운 프로젝트 생성

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+app.get('/', (req, res, next) => {
+    res.send('hi');
+});
+
+app.use((req, res, next) => {
+    const error = new Error(`${res.method} ${req.url} router doesn't exist`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_DEV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.send(res.locals.message);
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.get('port')} start server`);
+});
