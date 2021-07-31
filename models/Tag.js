074470d74ce30438095b23948eea42f9293176ab
skullKim <space1878@naver.com>
2021-07-31 22:25:11 +0900

    추가: 태그 테이블 추가 완료

---
+++ models/Tag.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Tag extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+           tag: {
+               type: Sequelize.STRING(45),
+               allowNull: false,
+           },
+        }, {
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: 'Tag',
+            tableName: 'tags',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+}
--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@ const Sequelize = require('sequelize');
 const User = require('./users');
 const Comment = require('./comments');
 const Posting = require('./postings');
+const PostingImage = require('./postingImage');
+const Tag = require('./Tag');
 
 const env = process.env.NODE_DEV || 'development';
 const config = require('../config/config')[env];
@@ -15,9 +17,13 @@ db.sequelize = sequelize;
 db.User = User;
 db.Comment = Comment;
 db.Posting = Posting;
+db.PostingImage =  PostingImage;
+db.Tag = Tag;
 
 User.init(sequelize);
 Comment.init(sequelize);
 Posting.init(sequelize);
+PostingImage.init(sequelize);
+Tag.init(sequelize);
 
 module.exports = db;
