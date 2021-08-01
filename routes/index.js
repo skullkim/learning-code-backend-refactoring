3e4965436d57c5459d34d4f48b5d9289399aa008
skullKim <space1878@naver.com>
2021-08-01 23:10:20 +0900

    추가: 응답할 json을 만들어 주는 함수 생성

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,19 @@ const express = require('express');
 const {Op} = require('sequelize');
 
 const User = require('../models/users');
+const {jsonResponse} = require('../lib/jsonResponse');
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try{
-        const {url, method, params, query, headers:{host, accept}} = req;
+        //const {url, method, params, query, headers:{host, accept}} = req;
         const users = await User.findAll({
             where: {id: {[Op.gt]: 0}},
         });
         res.setHeader('Content-Type', 'application/vnd.api+json');
         res.status(200);
-        const res_json = {
-            status: '200',
-            statusText: 'OK',
-            request: {
-                url,
-                method,
-                headers: {
-                    host,
-                    accept
-                },
-                params,
-                query,
-            },
-            data: users
-        }
-        res.json(res_json);
+        res.json(jsonResponse(req, users));
     }
     catch(err){
         console.error(err);
