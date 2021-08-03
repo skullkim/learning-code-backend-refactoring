2ec62da8dd0eeb57c6fc30a3998010410a9bc3ae
skullKim <space1878@naver.com>
2021-08-03 13:28:04 +0900

    추가: 메인 페이지에서 사용될 사진 응답 라우터 완료

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const AWS = require('aws-sdk');
 
 const {jsonResponse} = require('../lib/jsonResponse');
+const {AwsConfig} = require('../lib/awsConfig');
 
 const router = express.Router();
 
@@ -30,13 +32,13 @@ const router = express.Router();
 router.get('/main-page-images', (req, res, next) => {
     const resData = [
         {
-            img_url: process.env.MAIN_LOGO_KEY
+            img_url: '/main-page-image/Learning-Code'
         },
         {
-            img_url: process.env.MAIN_IMG2
+            img_url: '/main-page-image/main2'
         },
         {
-            img_url: process.env.MAIN_IMG3
+            img_url: '/main-page-image/programming-lan'
         }
     ];
     res.setHeader('Content-Type', 'application/vnd.api+json');
@@ -44,6 +46,23 @@ router.get('/main-page-images', (req, res, next) => {
     res.json(jsonResponse(req, resData));
 });
 
+router.get('/main-page-image/:image', async(req, res, next) => {
+    const s3 = new AWS.S3();
+    s3.getObject({
+        Bucket: `${process.env.AWS_S3_BUCKET}`,
+        Key: `upload/${req.params.image}.png`,
+    }, (err, data) => {
+        if(err) {
+            next(err);
+        }
+        else {
+            res.setHeader('Content-Type', 'image/png');
+            res.write(data.Body, 'binary');
+            res.end(null, 'binary');
+        }
+    });
+})
 
+router.get('/')
 
 module.exports = router;
