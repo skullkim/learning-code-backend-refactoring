5e031bf01b9beaff90487da677029b24731e4d51
skullKim <space1878@naver.com>
2021-08-03 19:41:50 +0900

    추가: auth router 추가

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ app.use(express.urlencoded({extended: true}));
 
 const index_router = require('./routes');
 const letters_router = require('./routes/letters');
+const auth_router = require('./routes/auth');
 
 app.use('/', index_router);
 app.use('/letters', letters_router);
+app.use('/authentication', auth_router);
 
 app.use((req, res, next) => {
     const error = new Error(`${res.method} ${req.url} router doesn't exist`);
