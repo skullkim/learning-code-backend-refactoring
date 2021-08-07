3181c7105732b01648d8b36f285884c8aa919de3
skullKim <kyk990328@gmail.com>
2021-08-07 14:32:16 +0900

    수정: 토큰 인증 미들웨어 수정 토큰을 찾지 못하는 문제 수정

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,22 +1,23 @@
 const jwt = require('jsonwebtoken');
 
-const {jsonErrorReponse, RESPONSE_ENUM} = require('../lib/jsonResponse');
+const {jsonErrorResponse, RESPONSE_ENUM} = require('../lib/jsonResponse');
 
-exports.verifyToken = (req, res, next) => {
+exports.verifyToken = async (req, res, next) => {
     try {
-        const token = req.headers.cookie.slice(4);
-        req.decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const {cookie} = req.headers;
+        const token = cookie.split('=')[1];
+        req.decoded = await jwt.verify(token, process.env.JWT_SECRET);
         req.data = req.decoded.data;
-        return next();
+        next();
     }
     catch(err) {
         if(err.name === 'TokenExpiredError') {
             res.setHeader('Content-Type', 'application/vnd.api+json');
             res.status(403);
-            return res.json(jsonErrorReponse(req, RESPONSE_ENUM[4], 403, 'Forbidden'));
+            return res.json(jsonErrorResponse(req, RESPONSE_ENUM[4], 403, 'Forbidden'));
         }
         res.setHeader('Content-Type', 'application/vnd.api+json');
         res.status(401);
-        return res.json(jsonErrorReponse(req, RESPONSE_ENUM[3], 401, 'Unauthorized'));
+        return res.json(jsonErrorResponse(req, RESPONSE_ENUM[3], 401, 'Unauthorized'));
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
+const {verifyToken} = require('./middleware');
+
 const router = express.Router();
 
-router.get('/profile/:userId', (req, res, next) => {
-    res.json({a: 1});
+router.get('/profile/:userId', verifyToken, (req, res, next) => {
+    const {userId} = req.params;
+    res.json({userId});
 });
 
 module.exports = router;
