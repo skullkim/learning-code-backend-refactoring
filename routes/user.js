1a163cf76f890d167f3571565a0a7fac2e17afa8
skullKim <kyk990328@gmail.com>
2021-08-08 15:39:36 +0900

    추가: 유저 프로필 이미지 s3에서 불러오기 완료

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const AWS = require('aws-sdk');
 const bcrypt = require('bcrypt');
 
+const {AwsConfig} = require('../lib/awsConfig');
 const {verifyToken} = require('./middleware');
 const {jsonResponse, jsonErrorResponse} = require('../lib/jsonResponse');
 const Posting = require('../models/postings');
@@ -36,6 +38,25 @@ router.get('/profile/:userId', verifyToken, async (req, res, next) => {
     }
 });
 
+router.get('/profile-image/:userId', verifyToken, (req, res, next) => {
+   const {profile_img_key} = req.decoded;
+   const s3 = new AWS.S3();
+   s3.getObject({
+       Bucket: `${process.env.AWS_S3_BUCKET}`,
+       Key: `${profile_img_key}`,
+   }, (err, data) => {
+       if(err) {
+           next(err);
+       }
+       else {
+           res.setHeader('Content-Type', 'image/png');
+           res.write(data.Body, 'binary');
+           res.end(null, 'binary');
+           }
+       });
+});
+
+
 router.put('/password/:userId', verifyToken, async(req, res, next) => {
     try{
        const {userId} = req.params;
@@ -64,6 +85,7 @@ router.put('/password/:userId', verifyToken, async(req, res, next) => {
     catch(err) {
        next(err);
     }
-})
+});
+
 
 module.exports = router;
