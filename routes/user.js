63095c7d81e112a17efc9332a81cf41e2adc7c56
skullKim <kyk990328@gmail.com>
2021-08-08 15:27:40 +0900

    추가: 프로필 페이지에서 비밀번호 바꾸기 추가

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const {verifyToken} = require('./middleware');
-const {jsonResponse} = require('../lib/jsonResponse');
+const {jsonResponse, jsonErrorResponse} = require('../lib/jsonResponse');
 const Posting = require('../models/postings');
 const Comment = require('../models/comments');
+const User = require('../models/users');
 
 const router = express.Router();
 
@@ -17,10 +19,10 @@ router.get('/profile/:userId', verifyToken, async (req, res, next) => {
         const comments = await Comment.findAll({
             where: {commenter_id: userId}
         });
-        const resPostings = postings.map(({dataValues:{title, main_category}}) => (
-            {title, main_category}
+        const resPostings = postings.map(({dataValues:{id, title, main_category}}) => (
+            {id, title, main_category}
         ));
-        const resComments = comments.map(({dataValues:{comment}}) => ({comment}));
+        const resComments = comments.map(({dataValues:{id, comment}}) => ({id, comment}));
         const responseData = {
             name,
             postings: resPostings,
@@ -32,5 +34,35 @@ router.get('/profile/:userId', verifyToken, async (req, res, next) => {
         next(err);
     }
 });
+
+router.put('/password/:userId', verifyToken, async(req, res, next) => {
+    try{
+       const {userId} = req.params;
+       const {prevPassword, newPassword} = req.body;
+       const exUser = await User.findOne({
+           where: {id: userId}
+       });
+       const comparePassword = await bcrypt.compare(prevPassword, exUser.password);
+       res.contentType('application/vnd.api+json');
+       if(!comparePassword) {
+          res.status(401);
+          return res.json(jsonErrorResponse(req,
+              {message: 'invalid password'},
+              401,
+              'Unauthorized'
+          ));
+       }
+       const hashedPassword = await bcrypt.hash(newPassword, 12);
+       await User.update(
+           {password: hashedPassword},
+           {where: {id: userId}}
+       );
+       res.status(201);
+       return res.json(jsonResponse(req, {message: 'success'}, 201, 'created'));
+    }
+    catch(err) {
+       next(err);
+    }
+})
 
 module.exports = router;
